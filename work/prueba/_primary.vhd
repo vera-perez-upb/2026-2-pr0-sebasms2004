c31bcf3775a0a2434d6ea03d5ae10a16ecf43e1d
Sebastian <sebasms2004@gmail.com>
2026-07-30 21:05:37 -0500

    Limpiar archivos temporales de Quartus, dejar solo entrega final

--- work/prueba/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity prueba is
-    port(
-        A               : in     vl_logic;
-        B               : in     vl_logic;
-        C               : out    vl_logic
-    );
-end prueba;
